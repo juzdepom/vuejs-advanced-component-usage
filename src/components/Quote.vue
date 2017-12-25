83d507346cdc4de32392f0ddce675676b45b6dc4
Julia Gao Miller <julia.gao.miller@gmail.com>
2017-12-25 01:12:41 -0800

    Initial commit

---
+++ src/components/Quote.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>A wonderful quote</p>
+  </div>
+
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+  div {
+    text-align: center;
+    border: 1px solid #ccc;
+    box-shadow: 1px, 1px, 2px, black;
+    padding: 30px;
+    margin: 30px auto;
+  }
+</style>
